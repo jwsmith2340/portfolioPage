1e1883076e2708c45f55bb1a3dc6357f48d36f3a
James Smith <jwsmith2340@yahoo.com>
2022-01-17 15:21:24 -0500

    first commit

---
+++ script/script.js
@@ -0,0 +1,50 @@
+$('#mob-header-hamburger-fa').on('click', openNav)
+
+$('.closeButton').on('click', closeNav)
+
+$('.first-a').on('click', scrollFirst)
+
+$('.second-a').on('click', scrollSecond)
+
+$('.third-a').on('click', scrollThird)
+
+$('.mob-header-logo-h3').on('click', scrollThirdFromLogo)
+
+$('.mob-top-box-intro-connect').on('click', scrollThird)
+
+function openNav() {
+    $('.sideMenu').css('width', '100%')
+}
+
+function closeNav() {
+    $('.sideMenu').css('width', '0')
+}
+
+function scrollFirst() {
+    $('.sideMenu').css('width', '0')
+    $('html').animate({
+        scrollTop: $('.mob-projects-title-h2').offset().top
+    }, 1000)
+}
+
+function scrollSecond() {
+    $('.sideMenu').css('width', '0')
+    $('html').animate({
+        scrollTop: $('.mob-skills-title-h2').offset().top
+    }, 1000)
+}
+
+function scrollThird() {
+    $('.sideMenu').css('width', '0')
+    $('html').animate({
+        scrollTop: $('.mob-contact-title-h2').offset().top
+    }, 1000)
+}
+
+function scrollThirdFromLogo() {
+    $('.sideMenu').css('width', '0')
+    $('.mob-header-logo-h3').toggleClass('mob-header-logo-h3-clicked')
+    $('html').animate({
+        scrollTop: $('.mob-contact-title-h2').offset().top
+    }, 1000)
+}
